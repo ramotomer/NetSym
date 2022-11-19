aa2cc90f6ea02c4d7c7273acbbfde2dda6a280db
tomer noyman <ramotomer@gmail.com>
2022-11-19 03:16:05 +0200

    Fix connection color bug...

--- src/NetSym/gui/tech/connection_graphics.py
+++ src/NetSym/gui/tech/connection_graphics.py
@@ -128,7 +128,7 @@ class ConnectionGraphics(ViewableGraphicsObject, DifferentColorWhenHovered, Sele
     def update_appearance(self) -> None:
         """Updates the color of the connection according to the PL and latency of the connection"""
         self.regular_color = CONNECTIONS.COLOR if not self.connection.packet_loss else CONNECTIONS.PL_COLOR
-        self.color = self.regular_color if all(not side.is_blocked for side in self.connection.get_sides()) else CONNECTIONS.BLOCKED_COLOR
+        self.color = self.color if all(not side.is_blocked for side in self.connection.get_sides()) else CONNECTIONS.BLOCKED_COLOR
 
         self.width = CONNECTIONS.DEFAULT_WIDTH if not self.connection.latency else CONNECTIONS.LATENCY_WIDTH
 
